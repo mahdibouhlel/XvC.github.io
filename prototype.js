8b01ed644be39c0f0a7c35ab045229e2cc40b8a9
mahdibouhlel <124781225+mahdibouhlel@users.noreply.github.com>
2023-03-14 08:25:08 +0100

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["livingrooms" , "bedrooms" , "diningrooms" , "gardin stuff" , "and some cool accessories"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeplay: 1000,
+    loop: true
+
+
+})
